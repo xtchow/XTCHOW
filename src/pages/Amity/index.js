30bfdc279f99bf06cd21cf2a7c356d835ee61c91
XTCHOW <xtchow2020@gmail.com>
2023-03-07 14:45:10 -0600

    overall feature backtracked

--- src/pages/Amity/index.js
+++
@@ -1,9 +0,0 @@
-// console.log('Hello world.');
-
-export default function Home() {
-  return (
-    <>
-      <p>from export: Hello world~!!</p>
-    </>
-  );
-};
--- src/pages/OS/index.js
+++
@@ -1,4 +0,0 @@
-
-export default function Campaigns() {
-  return (<p>Hello World~!</p>)
-}
